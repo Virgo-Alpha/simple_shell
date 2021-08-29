ea5187aef9a5cb5eb64ec346d1c972407456171b
Benson King'ori <79147634+Virgo-Alpha@users.noreply.github.com>
2021-08-29 17:20:13 +0400

    Update shell.h

--- shell.h
+++ shell.h
@@ -139,5 +139,7 @@ void help_setenv(void);
 void help_unsetenv(void);
 void help_history(void);
 
+int _signage(void);
+
 int proc_file_commands(char *file_path, int *exe_ret);
 #endif /* _SHELL_H_ */
